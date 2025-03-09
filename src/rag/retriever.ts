bf1fdddef37c23109ae4f42c8d4dc7b3bfd9919d
c0sm0thecoder <agazadekamal2004@gmail.com>
2025-03-09 15:30:05 +0400

    implement logic to retrieve the most relevant chunks to user query

---
+++ src/rag/retriever.ts
@@ -0,0 +1,34 @@
+import { generateEmbeddings } from "../config/llm";
+import { queryVectors } from "../db/pineconeClient";
+import { logger } from "../utils/logger";
+
+export interface QueryResult {
+    id: string;
+    metadata: any;
+    distance: number;
+}
+
+export async function retrieveRelevantChunks(
+    query: string,
+    topK: number = 5
+): Promise<QueryResult[]> {
+    try {
+        logger.info(`Retrieving relevant chunks for query: ${query}`);
+
+        const embeddedQuery = await generateEmbeddings(query);
+        const response = await queryVectors(embeddedQuery, topK);
+
+        const results: QueryResult[] = response.matches.map((match) => ({
+            id: match.id,
+            metadata: match.metadata,
+            distance: 1 - match.score,
+        }));
+
+        logger.info(`Retrieved relevant chunks for query: ${query}`);
+
+        return results;
+    } catch (error) {
+        logger.error(`Error retrieving relevant chunks: ${error}`);
+        throw new Error(`Error retrieving relevant chunks: ${error}`);
+    }
+}
